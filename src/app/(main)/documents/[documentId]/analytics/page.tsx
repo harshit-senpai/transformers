2daabe36ed9d4b04ca9b6495fb424b71db69815e
harshit Mishra <miananya2@gmail.com>
2024-12-12 16:36:03 +0530

    test

--- src/app/(main)/documents/[documentId]/analytics/page.tsx
+++ src/app/(main)/documents/[documentId]/analytics/page.tsx
@@ -55,9 +55,9 @@ export default function AnalyticsPage() {
   return (
     <main className="h-full">
       <h2 className="text-4xl font-bold">Analytics of Citizens Data</h2>
-      <section className="mt-4">
+      <section className="mt-4 h-96">
         <Textarea
-          className="w-full h-52 bg-background/40 border-border resize-none select-none cursor-default ring-0 focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+          className="w-full h-full overflow-y-auto bg-background/40 border-border resize-none select-none cursor-default ring-0 focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0"
           readOnly
           value={analysisData}
         />
